ea2a47de828e16ad80c4f4afbef99b95032ffd15
bogdan-chernenko <bogdan.chernenko.018@gmail.com>
2021-12-13 21:42:44 +0200

    update README

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ class BinaryDistribution(Distribution):
     def is_pure(self):
         return False
 
-__version__ = "1.0"
+__version__ = "0.1.0"
 
 setup(
-    name='Slash',
+    name='Slash92',
     version=__version__,
     description='ORM',
     author='Telegram: @M_O_D_E_R',
